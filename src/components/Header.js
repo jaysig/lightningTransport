2e46683cb2f530e9d9f901b64862a89b5e136538
Jaysig <jasontsigmon@gmail.com>
2018-03-31 11:52:41 -0500

    Updated the Header to the correct name

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,12 @@ let Header = (props) =>  {
           <ul className="content">
             <div>
               <li>
-                <h3>Lightning Transport</h3>
+                <h3 id="lighting">Lightning Transport</h3>
               </li>
             </div>
           </ul>
         </div>
   )
 }
+
+export default Header
