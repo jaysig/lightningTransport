c59109935a132f8c2b377974fdea142695cc8c51
Jaysig <jasontsigmon@gmail.com>
2018-03-31 12:34:46 -0500

    Able to get coordinates for a start and end point

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react';
 import MapForm from './MapForm'
+import { connect } from 'react-redux';
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import { newRoute } from '../actions';
 
 
 class MapContainer extends Component {
@@ -18,11 +20,41 @@ class MapContainer extends Component {
   handleFormSubmit = (event) => {
     event.preventDefault()
     console.log(this.state)
+    let startCords = {}
+    let endCords = {}
 
-    geocodeByAddress(this.state.address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error))
+    geocodeByAddress(this.state.start)
+      .then((results) => {
+        let geo = results[0].geometry.location
+        return getLatLng(results[0])
+      })
+      .then(({lat, lng}) => {
+        console.log('Success', lat, lng)
+        startCords = {
+          lat,
+          lng
+        }
+      })
+      .catch(error => console.error('Error at the Start', error))
+
+    geocodeByAddress(this.state.end)
+      .then((results) => {
+        let geo = results[0].geometry.location
+        return getLatLng(results[0])
+      })
+      .then(({lat, lng}) => {
+        endCords = {
+          lat,
+          lng
+        }
+        let cords = {
+          startCords,
+          endCords
+        }
+        this.props.dispatch(newRoute(cords))
+        console.log('End', startCords, endCords);
+      })
+      .catch(error => console.error('Error in the End', error))
   }
   render() {
     let start = {
@@ -33,6 +65,13 @@ class MapContainer extends Component {
       value: this.state.end,
       onChange: this.changeEnd,
     }
+    const cssClasses = {
+      root: 'ray', //Styles the
+      input: 'roy',
+      autocompleteContainer: 'places',
+      autocompleteItem: 'ron', //Colors the drop down items
+      //autocompleteItemActive: 'rob', //This doesn't appear linked to anything
+    }
     return(
       <div>
         <h3>Hello</h3>
@@ -41,10 +80,10 @@ class MapContainer extends Component {
           formSubmit={this.handleFormSubmit}
           start={start}
           end={end}
+          cssClasses={cssClasses}
         />
       </div>
     )
   }
 }
-
-export default MapContainer
+export default connect()(MapContainer)
--- src/components/MapForm.js
+++ src/components/MapForm.js
@@ -2,17 +2,23 @@ import React from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete'
 
 let MapForm = (props) => {
-  const {formSubmit, start, end } = props
+  const {formSubmit, start, end, cssClasses } = props
   return (
     <form onSubmit={formSubmit.bind(this)} id="valueForm" className="maxW">
       <h3>Where do you want to go?</h3>
       <div>
         <label> Start</label>
-        <PlacesAutocomplete inputProps={start}/>
+        <PlacesAutocomplete
+          inputProps={start}
+          classNames={cssClasses}
+        />
       </div>
       <div>
         <label>End</label>
-        <PlacesAutocomplete inputProps={end}/>
+        <PlacesAutocomplete
+          inputProps={end}
+          classNames={cssClasses}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
