bbef9fef1337fa92bd12c32b417adc1bc22ef800
Jaysig <jasontsigmon@gmail.com>
2018-04-03 16:24:22 +0800

    Fixed MapForm to not rely on the index.html file

--- src/components/MapForm.js
+++ src/components/MapForm.js
@@ -1,28 +1,68 @@
 import React from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete'
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap
+} = require("react-google-maps");
+import config from '../../config.json'
 
-let MapForm = (props) => {
-  const {formSubmit, start, end, cssClasses } = props
-  return (
-    <form onSubmit={formSubmit.bind(this)} id="valueForm">
-      <h3>Where do you want to go?</h3>
-      <div>
-        <label> Start</label>
-        <PlacesAutocomplete
-          inputProps={start}
-          classNames={cssClasses}
-        />
-      </div>
-      <div>
-        <label>End</label>
-        <PlacesAutocomplete
-          inputProps={end}
-          classNames={cssClasses}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+let MapForm = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.google.maps}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div />,
+    containerElement: <div />,
+    mapElement: <div />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+  lifecycle({
+  })
+)(props =>
+  <form onSubmit={props.formSubmit.bind(this)} id="valueForm">
+    <h3>Where do you want to go?</h3>
+    <div>
+      <label> Start</label>
+      <PlacesAutocomplete
+        inputProps={props.start}
+        classNames={props.cssClasses}
+      />
+    </div>
+    <div>
+      <label>End</label>
+      <PlacesAutocomplete
+        inputProps={props.end}
+        classNames={props.cssClasses}
+      />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+);
+
+
+
+// (props) => {
+//   const {formSubmit, start, end, cssClasses } = props
+//   return (
+//     <form onSubmit={formSubmit.bind(this)} id="valueForm">
+//       <h3>Where do you want to go?</h3>
+//       <div>
+//         <label> Start</label>
+//         <PlacesAutocomplete
+//           inputProps={start}
+//           classNames={cssClasses}
+//         />
+//       </div>
+//       <div>
+//         <label>End</label>
+//         <PlacesAutocomplete
+//           inputProps={end}
+//           classNames={cssClasses}
+//         />
+//       </div>
+//       <button type="submit">Submit</button>
+//     </form>
+//   )
+// }
 
 export default MapForm;
